368f71153c8218e350938db437c933a8bd42dcc7
Mark Sage <idaho.coder@gmail.com>
2018-07-13 16:47:24 -0500

    Display result of async calls

--- chapter6/template-driven/control-validity/src/app/stock/stock-list/stock-list.component.ts
+++ chapter6/template-driven/control-validity/src/app/stock/stock-list/stock-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Stock} from "../../model/stock";
 import {StockService} from "../../services/stock.service";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-stock-list',
@@ -9,18 +10,15 @@ import {StockService} from "../../services/stock.service";
 })
 export class StockListComponent implements OnInit {
 
-  public stocks: Stock[];
+  public stocks$: Observable<Stock[]>;
+  
   constructor(private stockService: StockService) { }
 
   ngOnInit() {
-    this.stockService.getStocks()
-      .subscribe(stocks => {
-        this.stocks = stocks;
-      });
+    this.stocks$ = this.stockService.getStocks();
   }
 
   onToggleFavorite(stock: Stock) {
-    console.log('Favorite for stock ', stock, ' was triggered');
     this.stockService.toggleFavorite(stock);
   }
 
