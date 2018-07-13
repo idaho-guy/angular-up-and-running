57b5c5f887ec7dbbb46db0039052f7cd590fbdff
Mark Sage <idaho.coder@gmail.com>
2018-07-13 16:08:28 -0500

    StockService updates

--- chapter6/template-driven/control-validity/src/app/stock/create-stock/create-stock.component.ts
+++ chapter6/template-driven/control-validity/src/app/stock/create-stock/create-stock.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from 'app/model/stock';
+import {StockService} from "../../services/stock.service";
 
 @Component({
   selector: 'app-create-stock',
@@ -10,8 +11,9 @@ export class CreateStockComponent {
 
   public stock: Stock;
   public confirmed = false;
+  public message = null;
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
-  constructor() {
+  constructor(private stockService: StockService) {
     this.stock =  new Stock('', '', 0, 0, 'NASDAQ');
   }
 
@@ -21,9 +23,16 @@ export class CreateStockComponent {
   }
 
   createStock(stockForm) {
-    console.log('Stock form', stockForm);
     if (stockForm.valid) {
-      console.log('Creating stock ', this.stock);
+      let created = this.stockService.createStock(this.stock);
+      if (created) {           4
+        this.message = 'Successfully created stock with stock code: '
+          + this.stock.code;
+        this.stock =  new Stock('', '', 0, 0, 'NASDAQ');
+      } else {
+        this.message = 'Stock with stock code: ' + this.stock.code
+          + ' already exists';
+      }
     } else {
       console.error('Stock form is in an invalid state');
     }
