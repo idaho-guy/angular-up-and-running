e4339f8a887a758fe9d9a577e918ee40ef851202
Mark Sage <idaho.coder@gmail.com>
2018-07-16 08:51:17 -0500

    Logging to show getting responses as text, events, and blob.

--- chapter6/template-driven/control-validity/src/app/services/stock.service.ts
+++ chapter6/template-driven/control-validity/src/app/services/stock.service.ts
@@ -4,7 +4,7 @@ import {Observable} from 'rxjs/Observable';
 import {_throw as ObservableThrow} from 'rxjs/observable/throw';
 import {of as ObservableOf} from 'rxjs/observable/of';
 import {Stock} from 'app/model/stock';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpEvent, HttpHeaders, HttpResponse} from "@angular/common/http";
 
 @Injectable()
 export class StockService {
@@ -23,18 +23,32 @@ export class StockService {
       params: {
         q: 'test',
         test: 'value'
-      }
+      },
+      observe: 'body'
     });
   }
 
-  createStock(stock: Stock): Observable<any> {
-    return this.http.post('/api/stock', stock);
+  getStocksAsResponse(): Observable<HttpResponse<Stock[]>> {
+    return this.http.get<Stock[]>('/api/stock', {
+      observe: 'response'
+    });
   }
 
-  toggleFavorite(stock: Stock): Observable<Stock> {
-    return this.http.patch<Stock>('/api/stock/' + stock.code,
-      {
-        favorite: !stock.favorite
-      });
+  getStocksAsEvents(): Observable<HttpEvent<any>> {
+    return this.http.get('/api/stock', {
+      observe: 'events'
+    })
+  }
+
+  getStocksAsString(): Observable<string> {
+    return this.http.get('/api/stock', {
+      responseType: 'text'
+    });
+  }
+
+  getStocksAsBlob(): Observable<Blob> {
+    return this.http.get('/api/stock', {
+      responseType: 'blob'
+    });
   }
 }
