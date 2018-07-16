cebc63d5b238b7686d951b4894e984a0425261d7
Mark Sage <idaho.coder@gmail.com>
2018-07-16 08:41:52 -0500

    format

--- chapter6/template-driven/control-validity/src/app/services/stock.service.ts
+++ chapter6/template-driven/control-validity/src/app/services/stock.service.ts
@@ -1,33 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import { _throw as ObservableThrow } from 'rxjs/observable/throw';
-import { of as ObservableOf } from 'rxjs/observable/of';
-import { Stock } from 'app/model/stock';
+import {Observable} from 'rxjs/Observable';
+import {_throw as ObservableThrow} from 'rxjs/observable/throw';
+import {of as ObservableOf} from 'rxjs/observable/of';
+import {Stock} from 'app/model/stock';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class StockService {
 
   private stocks: Stock[];
+
   constructor(private http: HttpClient) {
 
   }
 
-  getStocks() : Observable<Stock[]> {
+  getStocks(): Observable<Stock[]> {
     return this.http.get<Stock[]>('/api/stock', {
-        headers: new HttpHeaders()
+      headers: new HttpHeaders()
         .set('Authorization', 'MyAuthorizationHeaderValue')
         .set('X-EXAMPLE-HEADER', 'TestValue'),
       params: {
-      q: 'test',
+        q: 'test',
         test: 'value'
-    }
-  });
+      }
+    });
   }
 
   createStock(stock: Stock): Observable<any> {
-    return this.http.post('/api/stock',stock);
+    return this.http.post('/api/stock', stock);
   }
 
   toggleFavorite(stock: Stock): Observable<Stock> {
