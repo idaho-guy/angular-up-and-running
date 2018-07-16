15f6ef82f750190ea71bbcecabe23e14d89ec3f1
Mark Sage <idaho.coder@gmail.com>
2018-07-16 08:36:08 -0500

    add options to httpclient call: 'q' is used for search string

--- chapter6/template-driven/control-validity/src/app/services/stock.service.ts
+++ chapter6/template-driven/control-validity/src/app/services/stock.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { _throw as ObservableThrow } from 'rxjs/observable/throw';
 import { of as ObservableOf } from 'rxjs/observable/of';
 import { Stock } from 'app/model/stock';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class StockService {
@@ -15,7 +15,15 @@ export class StockService {
   }
 
   getStocks() : Observable<Stock[]> {
-    return this.http.get<Stock[]>('/api/stock');
+    return this.http.get<Stock[]>('/api/stock', {
+        headers: new HttpHeaders()
+        .set('Authorization', 'MyAuthorizationHeaderValue')
+        .set('X-EXAMPLE-HEADER', 'TestValue'),
+      params: {
+      q: 'test',
+        test: 'value'
+    }
+  });
   }
 
   createStock(stock: Stock): Observable<any> {
